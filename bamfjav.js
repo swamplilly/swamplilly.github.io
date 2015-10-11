df914ae1dbd8b5bc5a7f1c40dc6d40d35f90e9ab
Lilly An <li.liwei94@gmail.com>
2015-10-11 10:54:17 -0700

    fuuckkkk

--- bamfjav.js
+++ bamfjav.js
@@ -1,6 +1,43 @@
 window.onload = function()
 {
 	var x = document.getElementById("geotest");
+	var lat, lon;
+
+	function getLocation()
+	{
+		if (navigator.geolocation)
+		{
+			navigator.geolocation.getCurrentPosition(showPosition, error);
+		} else
+		{ 
+			x.innerHTML = "Geolocation is not supported by this browser.";
+		}
+	}
+
+	function showPosition(position)
+	{
+		lat = position.coords.latitude;
+		lon = position.coords.longitude;
+		//x.innerHTML = "Latitude: " + lat + "<br>Longitude: " + lon;
+		var httpRequest = new XMLHttpRequest();
+		// httpRequest.onreadystatechange = function()
+		// {
+		// 	if (httpRequest.readyState === 4)
+		// 	{
+		// 		alert("done");
+		// 	}
+		// };
+		httpRequest.open('POST', "http://localhost/api", true);
+		httpRequest.setRequestHeader('Content-Type', 'application/json');
+		var coords = {lat: position.coords.latitude, lon: position.coords.longitude}
+		httpRequest.send(JSON.stringify(coords));
+		// alert(JSON.stringify(coords));
+	}
+
+	function error()
+	{
+
+	}
 
 	function getRandomColor()
 	{
